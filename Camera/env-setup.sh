4ef8a2aa83263273ebb4ef43d953361b62d3deff
Yannsean22 <69013397+Yannsean22@users.noreply.github.com>
2025-01-12 03:32:20 -0500

    Update env-setup.sh

--- Camera/env-setup.sh
+++ Camera/env-setup.sh
@@ -17,4 +17,5 @@ echo '--ZAIRE HELMETS CAMERA AND GPIOs REQ COMPLETED--'
 
 ##use
 #ffmpeg -f v4l2 -framerate 25 -video_size 640x480  -i /dev/video0 -c:v libx264 -preset veryfast -f mp4 ~/Desktop/video.mp4
-
+#to be test
+#fast
