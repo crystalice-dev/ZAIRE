df2f5a1ca57a004cb51becb32ceaf1a56fdd90db
Yannsean22 <69013397+Yannsean22@users.noreply.github.com>
2025-03-22 15:02:31 -0400

    Update env-setup.sh

--- CAMERA_HUB/env-setup.sh
+++ CAMERA_HUB/env-setup.sh
@@ -25,3 +25,30 @@ echo '--ZAIRE HELMETS CAMERA AND GPIOs REQ COMPLETED--'
 ##gcc -o main main.c -lwiringPi -lm -lpthread -lrt -lcrypt
 #to be test
 #fast
+: <<'END_COMMENT'
++-----+-----+---------+------+---+---Pi ---+---+------+---------+-----+-----+
+| CPU | wPi |    Name   | Mode | V | Physical | V | Mode |   Name    | wPi | CPU |
++-----+-----+-----------+------+---+----++----+---+------+-----------+-----+-----+
+|     |     |      3.3v |      |   |  1 || 2  |   |      | 5v        |     |     |
+|  12 |   8 |     SDA.1 | ALT3 | 0 |  3 || 4  |   |      | 5V        |     |     |
+|  11 |   9 |     SCL.1 | ALT3 | 0 |  5 || 6  |   |      | GND       |     |     |
+|   6 |   7 |      PWM1 | ALT3 | 0 |  7 || 8  | 0 | ALT3 | UART3-TX  | 15  | 13  |
+|     |     |       GND |      |   |  9 || 10 | 0 | ALT3 | UART3-RX  | 16  | 14  |
+|   1 |   0 |  UART2-RX | ALT3 | 0 | 11 || 12 | 0 | ALT3 | UART3-CTS | 1   | 16  |
+|   0 |   2 |  UART2-TX | ALT3 | 0 | 13 || 14 |   |      | GND       |     |     |
+|   3 |   3 | UART2-CTS | ALT3 | 0 | 15 || 16 | 0 | ALT3 | UART3-RTS | 4   | 15  |
+|     |     |      3.3v |      |   | 17 || 18 | 0 | ALT3 | GPIO.PC04 | 5   | 68  |
+|  64 |  12 | SPI0_MOSI | ALT3 | 0 | 19 || 20 |   |      | GND       |     |     |
+|  65 |  13 | SPI0_MISO | ALT3 | 0 | 21 || 22 | 0 | ALT3 | UART2-RTS | 6   | 2   |
+|  66 |  14 |  SPI0_CLK | ALT3 | 0 | 23 || 24 | 0 | ALT3 | SPI0-CS   | 10  | 67  |
+|     |     |       GND |      |   | 25 || 26 | 0 | ALT3 | GPIO.PL07 | 11  | 71  |
+|  19 |  30 |     SDA.0 | ALT3 | 0 | 27 || 28 | 0 | ALT3 | SCL.0     | 31  | 18  |
+|   7 |  21 | GPIO.PA07 | ALT3 | 0 | 29 || 30 |   |      | GND       |     |     |
+|   8 |  22 | GPIO.PA08 | ALT3 | 0 | 31 || 32 | 0 | ALT3 | GPIO.PL02 | 26  | 354 |
+|   9 |  23 | GPIO.PA09 | ALT3 | 0 | 33 || 34 |   |      | GND       |     |     |
+|  10 |  24 | GPIO.PA10 | ALT3 | 0 | 35 || 36 | 0 | ALT3 | GPIO.PL04 | 27  | 356 |
+|  17 |  25 | SPDIF-OUT | ALT3 | 0 | 37 || 38 | 0 | ALT3 | GPIO.PA21 | 28  | 21  |
+|     |     |       GND |      |   | 39 || 40 | 0 | ALT3 | GPIO.PA20 | 29  | 20  |
++-----+-----+---------+------+---+---Pi ---+---+------+---------+-----+-----+
+END_COMMENT
+
