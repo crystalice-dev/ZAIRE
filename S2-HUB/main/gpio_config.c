34cd71d31cd5915a69ef000c334ea4e6685d9731
Yannsean22 <69013397+Yannsean22@users.noreply.github.com>
2025-05-05 01:20:45 -0400

    Delete S2-HUB/main directory

--- S2-HUB/main/gpio_config.c
+++
@@ -1,9 +0,0 @@
-#include <globalVar.h>
-
-
-
-esp_err_t gpio_pin_init(void){
-    esp_err_t err;
-
-    return ESP_OK;
-}
--- S2-HUB/main/i2c_device_config.c
+++
@@ -1,6 +0,0 @@
-#include <globalVar.h>
-
-
-
-uint8_t BATTERY_STATUS = 0;
-uint8_t TEMP_STATUS = 0;
